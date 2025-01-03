0935ebcbc21529cabd3b404790abb79a21a30032
Qazi Sajid Azam <azam.qsa@gmail.com>
2025-01-03 19:43:08 +0530

    initial commit

---
+++ generate_ini.py
@@ -0,0 +1,111 @@
+import sys
+import os
+
+iniSeparator = ";"+("-"*20)
+
+def fixName(name):
+    parts = name.split("-")
+    for i in range(len(parts)):
+        parts[i] = parts[i].title()
+    return ''.join(parts)
+
+def extractHash(name):
+    filename = os.listdir(f"original/{name}")[0]
+    return filename.split("-")[0]
+
+def getResourcePath(name):
+    files = os.listdir(f"resources/{name}")
+    if(len(files)>0):
+        return f".\\resources\\{name}\\{files[0]}"
+    return ''
+
+def generateIniSection(name):
+    sectionName = fixName(name)
+    hash = extractHash(name)
+    resourcePath = getResourcePath(name)
+    if (resourcePath == ''):
+        lines = [
+            f";[TextureOverride{sectionName}]",
+            f";hash = {hash}",
+            f";ps-t0 = Resource{sectionName}",
+            ";",
+            f";[Resource{sectionName}]",
+            f";filename = {resourcePath}",
+        ]
+    else:
+        lines = [
+            f"[TextureOverride{sectionName}]",
+            f"hash = {hash}",
+            f"ps-t0 = Resource{sectionName}",
+            "",
+            f"[Resource{sectionName}]",
+            f"filename = {resourcePath}",
+        ]
+    return lines
+
+def findDuplicateSection(sections, section):
+    override = ""
+    for line in section:
+        if "TextureOverride" in line:
+            override = line.strip().replace(";", "")
+    for s in sections:
+        for line in s:
+            if override.strip() in line:
+                #print("preserving section "+override)
+                trimmed = []
+                for l in s:
+                    sl = l.strip()
+                    if len(sl) > 1:
+                        trimmed.append(l.strip())
+                trimmed = trimmed[0:3]+[""]+trimmed[3:]
+                return trimmed
+    print("adding new section "+override)
+    return section
+
+def parseFile(file):
+    section = []
+    sections = []
+    for line in file:
+        if iniSeparator in line:
+            sections.append(section)
+            section = []
+        else:
+            section.append(line)
+    print(f"found {len(sections)} sections")
+    return sections
+    
+
+original_names = os.listdir("original")
+args = sys.argv
+if (len(args)>1):
+    original_names = args[1:]
+
+files = os.listdir(".")
+isFirst = True
+if "mod.ini" in files:
+    print("updating old file")
+    iniFile = open("mod.ini", "r")
+    oldsections = parseFile(iniFile)
+    iniFile.close()
+    os.remove("mod.ini")
+    iniFile = open("mod.ini", "a")
+    for name in original_names:
+        section = generateIniSection(name)
+        section = findDuplicateSection(oldsections, section)
+        if not isFirst:
+            iniFile.write('\n')
+        for line in section:
+            iniFile.write(line+'\n')
+        iniFile.write('\n'+iniSeparator+'\n')
+        isFirst = False
+else:
+    print("generating new file")
+    iniFile = open("mod.ini", "a")
+    for name in original_names:
+        section = generateIniSection(name)
+        if not isFirst:
+            iniFile.write('\n')
+        for line in section:
+            iniFile.write(line+'\n')
+        iniFile.write('\n'+iniSeparator+'\n')
+        isFirst = False
---
+++ generate_resources.py
@@ -0,0 +1,63 @@
+import sys
+import os
+import subprocess
+from PIL import Image
+
+def generate_dds(input_path, output_path, output_index):
+    try:
+        output_file_png = output_path + "/" + str(output_index) + ".png"
+        output_file_dds = output_path + "/" + str(output_index) + ".dds"
+        output_file_dds = output_file_dds.replace('/', '\\')
+        img = Image.open(input_path)
+        img = resize(img)
+        img.save(output_file_png, 'PNG', srgb=False)
+        subprocess.run(["texconv.exe", "-f", "BC7_UNORM", "-y", "-sepalpha", "-srgb", "-m", "1", "-o", ".", output_file_png])
+        os.remove(output_file_png)
+    except:
+        return
+    return
+
+def resize(image):
+    image = image.transpose(Image.FLIP_TOP_BOTTOM)
+    width, height = image.size
+    target_width, target_height = 256, 256
+    aspect_ratio = width / height
+
+    if aspect_ratio > 1:
+        # Then crop the left and right edges:
+        new_width = height
+        offset = (width - new_width) / 2
+        resize = (offset, 0, width - offset, height)
+    else:
+        # ... crop the top and bottom:
+        new_height = width
+        offset = (height - new_height) / 2
+        resize = (0, offset, width, height - offset)
+
+    thumb = image.crop(resize).resize((target_width, target_height), Image.ANTIALIAS)
+    return thumb
+
+
+os.makedirs(f"resources", exist_ok=True)
+original_names = os.listdir("original")
+args = sys.argv
+force = args[1:]
+
+for name in original_names:
+    os.makedirs(f"resources/{name}", exist_ok=True)
+    input_files = os.listdir(f"source/{name}")
+    output_files = os.listdir(f"resources/{name}")
+    if len(input_files) == 0:
+        continue
+    index = 0
+    if len(input_files) <= len(output_files) and name not in force:
+        continue
+    print(f"generating resources for {name}")
+    for filename in input_files:
+        generate_dds(
+            input_path=f"source/{name}/{filename}",
+            output_path=f"resources/{name}",
+            output_index=str(index)
+            )
+        index += 1
+    break
